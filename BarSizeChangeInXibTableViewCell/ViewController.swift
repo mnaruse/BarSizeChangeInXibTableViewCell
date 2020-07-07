98ae7c8a408b59df7f2a193a544e95b128d286e1
Miharu Naruse <nars024@gmail.com>
2020-07-08 05:13:36 +0900

    Initial Commit

---
+++ BarSizeChangeInXibTableViewCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BarSizeChangeInXibTableViewCell
+//
+//  Created by MiharuNaruse on 2020/07/08.
+//  Copyright © 2020 m_rn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
